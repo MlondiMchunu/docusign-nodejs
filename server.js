ba97ae2242f5455ba5ef46c972c95143b7c25417
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-09-09 22:25:44 +0200

    add env variables

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const path = require("path");
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv")
 
 const app = express();
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
+dotenv.config();
 
-app.post("/form",(req,res)=>{
-console.log("received form data",req.body);
-res.send("Form Received!")
+app.post("/form", (req, res) => {
+    const data = req.body
+  console.log("received form data", req.body);
+  //res.send({ "Form Data": req.body });
+  res.send({"form data":data})
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "main.html"))
+  res.sendFile(path.join(__dirname, "main.html"));
 });
 
 app.listen(3000, () => {
