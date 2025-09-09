4d9d5db1c2b0e529cc82ff89eb2db5c321ac9dbb
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-09-09 17:43:24 +0200

    update form and css

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const path=require('path')
+const express = require("express");
+const path = require("path");
 
-const app = express()
+const app = express();
 
-app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname, "main.html"))
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "main.html"))
+});
 
-app.listen(3000,()=>{
-    console.log(`Server is running on port 3000`)
-})
+app.listen(3000, () => {
+  console.log(`Server is running on port 3000`);
+});
