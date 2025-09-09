3fe5bdfca7ff08e9a8fb1c939f089ff2496b9190
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-09-09 20:14:10 +0200

    add body-parser to server

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser")
 
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}))
 
 
-app.post("/",(req,res)=>{
-
+app.post("/form",(req,res)=>{
+console.log("received form data",req.body);
+res.send("Form Received!")
 });
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "main.html"))
 });
