d239b7aa059e0718f664e5f586a0759d30d4e7df
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-09-15 16:24:54 +0200

    create envelope definition from a template

--- server.js
+++ server.js
@@ -43,6 +43,45 @@ getEnvelopesApi = (req) => {
   return new docusign.EnvelopesApi(dsApiClient);
 };
 
+function makeEnvelope(args) {
+  // Data for this method
+  // args.signerEmail
+  // args.signerName
+  // args.ccEmail
+  // args.ccName
+  // args.templateId
+
+  // The envelope has two recipients.
+  // recipient 1 - signer
+  // recipient 2 - cc
+
+  // create the envelope definition
+  let env = new docusign.EnvelopeDefinition();
+  env.templateId = args.templateId;
+
+  // Create template role elements to connect the signer and cc recipients
+  // to the template
+  // We're setting the parameters via the object creation
+  let signer1 = docusign.TemplateRole.constructFromObject({
+    email: args.signerEmail,
+    name: args.signerName,
+    roleName: 'signer',
+  });
+
+  // Create a cc template role.
+  // We're setting the parameters via setters
+  let cc1 = new docusign.TemplateRole();
+  cc1.email = args.ccEmail;
+  cc1.name = args.ccName;
+  cc1.roleName = 'cc';
+
+  // Add the TemplateRole objects to the envelope object
+  env.templateRoles = [signer1, cc1];
+  env.status = 'sent'; // We want the envelope to be sent
+
+  return env;
+}
+
 checkToken = async (req) => {
   if (req.session.access_token && Date.now() < req.session.expires_at) {
     console.log("re-using access_token", req);
